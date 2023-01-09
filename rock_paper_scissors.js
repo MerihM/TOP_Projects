68ccc7b124ff198a26b24b863878fb31ac6ba4a4
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-09 13:29:05 +0100

    Refactor logic and set goal

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,125 +1,72 @@
-
-// Function that returns computer choice
-
-function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * 3) + 1;
-    return choiceName(compChoice);
-}
-
-// Function that returns name of choice as a string
-
-function choiceName(num) {
-    switch (num) {
-        case 1:
-            return 'Rock';
-        case 2:
-            return 'Paper';
-        case 3:
-            return 'Scissors';
-        default:
-            return 'Error';
-    }
-}
-
-// Function that plays one round of Rock-Paper-Scissors
-
-
-let resultPlayer = 0, resultComputer = 0;
-
-function playOneRound(playerSelection, computerSelection) {
-
-    let player, playerLower = playerSelection.toLowerCase(), numberPlayer, numberComputer;
-    if (playerLower === 'rock')
-        player = choiceName(1);
-    else if (playerLower === 'paper')
-        player = choiceName(2);
-    else
-        player = choiceName(3);
-
-    const outcomeOne = 'Rock beats Scissors', outcomeTwo = 'Paper beats Rock', outcomeThree = 'Scissors beat Paper';
-
-    // TODO optimize logic, conditions too long
-    if (player === computerSelection)
-        console.log("Draw!!");
-    else {
-        if ((player === 'Scissors' && computerSelection === 'Rock') || (player === 'Rock' && computerSelection === 'Scissors')) {
-            if (player === 'Scissors') {
-                console.log('Lose!!! ' + outcomeOne);
-                resultComputer++;
-            }
-
-            else {
-                console.log('Win!!! ' + outcomeOne);
-                resultPlayer++;
-            }
-
-        }
-        else if ((player === 'Paper' && computerSelection === 'Rock') || (player === 'Rock' && computerSelection === 'Paper')) {
-            if (player === 'Rock') {
-                console.log('Lose!!! ' + outcomeTwo);
-                resultComputer++;
-            }
-
-            else {
-                console.log('Win!!! ' + outcomeTwo);
-                resultPlayer++;
-            }
-
-        }
-        else {
-            if (player === 'Paper') {
-                console.log('Lose!!! ' + outcomeThree);
-                resultComputer++;
-            }
-
-            else {
-                console.log('Win!!! ' + outcomeThree);
-                resultPlayer++;
-            }
-
-        }
-    }
-}
-
-
-// function game() {
-
-//     for (let i = 0; i < 5; i++)
-//         playOneRound(prompt('Enter selection'), getComputerChoice());
-
-//     if (resultComputer > resultPlayer)
-//         console.log(`Computer wins with result : ${resultComputer} - ${resultPlayer}`);
-//     else if (resultPlayer > resultComputer) {
-//         console.log(`Player wins with result ${resultPlayer} - ${resultComputer}`);
-//     }
-//     else
-//         console.log(`Draw with result : ${resultComputer} - ${resultPlayer}`);
-
-//     resultComputer = resultPlayer = 0;
-// }
-
-
-
 const btnSelection = document.querySelectorAll('button');
 const scorePlayer = document.querySelector('#scorePlayer');
 const scoreComputer = document.querySelector('#scoreComputer');
+let resComp = 0, resPla = 0;
+
+
+// Event for button click, get result of rock-paper-scissors game, announces winner when someone gets to 5 points
+
 
 btnSelection.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        playOneRound(btn.getAttribute('name'), getComputerChoice());
-        console.log(resultComputer);
-        console.log(resultPlayer);
-        scoreComputer.innerText = resultComputer;
-        scorePlayer.innerText = resultPlayer;
+        let choicePla = btn.getAttribute('name') * 1;
+        playGame(choicePla, getComputer());
+        if ((resComp === 5) || (resPla === 5))
+            announceWinner();
+        scoreComputer.innerText = resComp;
+        scorePlayer.innerText = resPla;
     })
 })
 
 
-function playGame(player, computer) {
-    let resComp = 0, resPla = 0;
+// Function that plays round of rock-paper-scissors, and adjusts result
 
-    if (player === computer)
+
+function playGame(player, computer) {
+    let lower = Math.min(player, computer), higher = Math.max(player, computer);
+
+    if (player === computer) {
+        console.log('Draw');
         return;
-    if (player !== 0 && player < computer)
-        resComp++;
+    }
+    if ((higher - lower) !== 2) {
+        if (player > computer) {
+            resPla++;
+            console.log('Player wins');
+        }
+        else {
+            resComp++;
+            console.log('Computer wins');
+        }
+    }
+    else {
+        if (player === 0) {
+            resPla++;
+            console.log('Player wins');
+        }
+        else {
+            resComp++;
+            console.log('Computer wins');
+        }
+    }
+}
+
+
+//Function that returns computer choice for rock-paper-scissors game
+
+
+function getComputer() {
+    return Math.floor(Math.random() * 3);
+}
+
+
+// Function that announces winner and resets score to 0
+
+
+function announceWinner() {
+    if (resComp === 5)
+        alert('COMPUTER WINS');
+    else
+        alert('PLAYER WINS');
+    resComp = resPla = 0;
 }
