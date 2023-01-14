b6eca2b4c37b841e12c55ca8609740281b51deb9
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-14 14:33:38 +0100

    Calculate result with operator

--- calc.js
+++ calc.js
@@ -4,7 +4,7 @@ const lowerScreen = document.querySelector('.letterBig');
 const clear = document.querySelector('.clear');
 const btnOperators = document.querySelectorAll('.operators');
 const equal = document.querySelector('.equal');
-let first, second, operationBefore = false, operation = '', opCalled = false;
+let first, second, operationBefore = false, operation = '', operatorCalled = false;
 
 
 /*
@@ -40,46 +40,57 @@ for (let btn of buttons) {
 */
 
 
-function operatorCalculation(op) {
-    first = lowerScreen.innerText * 1;
+function assignmentScreens(op) {
     operation = op.value;
     upperScreen.innerText = (first + " " + op.innerText);
     operationBefore = true;
-    opCalled = true;
+    operatorCalled = true;
+}
+
+function operatorCalculation(op) {
+
+    if (!operatorCalled) {
+        first = lowerScreen.innerText * 1;
+        assignmentScreens(op);
+    }
+    else {
+        first = switchOperation();
+        assignmentScreens(op);
+    }
+}
+
+function switchOperation() {
+    second = lowerScreen.innerText * 1;
+    switch (operation) {
+        case '+':
+            return first + second;
+        case '-':
+            return first - second;
+        case 'div':
+            return first / second;
+        case 'time':
+            return (first * second);
+    }
 }
 
 function equals() {
-    let num;
-    second = lowerScreen.innerText * 1;
+    let num = Math.round(switchOperation() * 100) / 100;
 
-    switch (operation) {
-        case '+':
-            num = first + second;
-            lowerScreen.innerText = Math.round(num * 100) / 100;
-            upperScreen.innerText += " " + second + " " + " = ";
-            break;
-        case '-':
-            num = first - second;
-            lowerScreen.innerText = Math.round(num * 100) / 100;
-            upperScreen.innerText += " " + second + " " + " = ";
-            break;
-        case 'time':
-            num = first * second;
-            lowerScreen.innerText = Math.round(num * 100) / 100;
-            upperScreen.innerText += " " + second + " " + " = ";
-            break;
-        case 'div':
-            num = (first / second);
-            lowerScreen.innerText = Math.round(num * 100) / 100;
-            upperScreen.innerText += " " + second + " " + " = ";
-            break;
+    if (!num) {
+        lowerScreen.innerText = '';
+        upperScreen.innerText = '';
+        return;
     }
+
+    lowerScreen.innerText = num;
+    upperScreen.innerText += " " + second + " " + " = ";
+    if (operatorCalled)
+        operatorCalled = false;
 }
 
 function writeNumber(e) {
-    if (operationBefore) {
+    if (operationBefore)
         lowerScreen.innerText = '';
-    }
     lowerScreen.innerText += e.innerText;
     operationBefore = false;
 }
