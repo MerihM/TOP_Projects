13d987a134b04df9d34e4c5e4ed8234bc5e00883
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-14 10:56:42 +0100

    Add func to write operations

--- calc.js
+++ calc.js
@@ -2,6 +2,38 @@ const buttons = document.querySelectorAll('.numbers');
 const upperScreen = document.querySelector('.letterSmall');
 const lowerScreen = document.querySelector('.letterBig');
 const clear = document.querySelector('.clear');
+const btnOperators = document.querySelectorAll('.operators');
+let first, second, operationBefore = false;
+
+for (let e of btnOperators) {
+    e.addEventListener('click', () => operatorCalculation(e));
+}
+
+function operatorCalculation(op) {
+    if (!lowerScreen.innerText)
+        return;
+    console.log('I CALCULATE ' + op.innerText);
+    if (!first)
+        first = lowerScreen.innerText * 1;
+    upperScreen.innerText = lowerScreen.innerText + ' ' + op.innerText;
+    operationBefore = true;
+    console.log(first);
+}
+
+function equals(nu1, nu2, operator) {
+    switch (operator) {
+        case 'div':
+            return (nu1 / nu2);
+        case 'time':
+            return (nu1 * nu2);
+        case '+':
+            return (nu1 + nu2);
+        case '-':
+            return (nu1 - nu2);
+        default:
+            return;
+    }
+}
 
 clear.addEventListener('click', () => {
     lowerScreen.innerText = '';
@@ -13,7 +45,9 @@ for (let btn of buttons) {
 }
 
 function writeNumber(e) {
-    upperScreen.innerText += e.innerText;
-    lowerScreen.innerText = e.innerText;
-    // console.log(e.innerText);
+    if (operationBefore) {
+        lowerScreen.innerText = '';
+    }
+    lowerScreen.innerText += e.innerText;
+    operationBefore = false;
 }
