a1d8a35f8f1e48bbb5f7f0a8fe8863c9ecf24338
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-19 09:22:47 +0100

    Find best pair

--- stock.rb
+++ stock.rb
@@ -2,20 +2,33 @@
 def number_of_arrays(num) 
     return ((num * (num + 1))/2)
 end
-def stock_picker(input_arr)
-    num_of_arrays = number_of_arrays(input_arr.length - 1)
+
+def populate_array(arr)
+    num_of_arrays = number_of_arrays(arr.length - 1)
     array_of_values = Array.new(num_of_arrays) {Array.new()}
     arr_o_val_index = 0
-    input_arr.each_with_index do
+    arr.each_with_index do
         |value, index|
-        for i in index+1...input_arr.length
-            array_of_values[arr_o_val_index].push(value - input_arr[i])
+        for i in index+1...arr.length
+            array_of_values[arr_o_val_index].push(value - arr[i])
             array_of_values[arr_o_val_index].push(index)
             array_of_values[arr_o_val_index].push(i)
             arr_o_val_index += 1
         end
     end
-    p array_of_values
+    array_of_values
+end
+
+def stock_picker(input_arr)
+    
+    temp_arr = populate_array(input_arr)
+    max_val = temp_arr[0]
+    temp_arr.each do 
+        |arr|
+        max_val = arr if arr[0] >= max_val[0]
+    end
+    max_val.shift
+    p max_val
 end
 
 
