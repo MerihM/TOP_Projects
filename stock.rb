e33dcde62559329c5dc473723d119a0b623100fb
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-19 09:11:33 +0100

    Populate array

--- stock.rb
+++ stock.rb
@@ -3,15 +3,19 @@ def number_of_arrays(num)
     return ((num * (num + 1))/2)
 end
 def stock_picker(input_arr)
-    num_of_arrays = number_of_arrays(input_arr.length)
-    array_of_pairs = Array.new(num_of_arrays) {Array.new(2, 0)}
-    array_of_indices = Array.new(num_of_arrays) {Array.new(2, 0)}
-    p array_of_indices
-    p array_of_pairs
-    # Make array of pairs, there will be ((array.length * (array.length + 1))/2 )pairs,
-    # sum of all numbers from 1 to array.length
-    # Make array of indices
-
+    num_of_arrays = number_of_arrays(input_arr.length - 1)
+    array_of_values = Array.new(num_of_arrays) {Array.new()}
+    arr_o_val_index = 0
+    input_arr.each_with_index do
+        |value, index|
+        for i in index+1...input_arr.length
+            array_of_values[arr_o_val_index].push(value - input_arr[i])
+            array_of_values[arr_o_val_index].push(index)
+            array_of_values[arr_o_val_index].push(i)
+            arr_o_val_index += 1
+        end
+    end
+    p array_of_values
 end
 
 
