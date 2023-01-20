e0e8ee40f035e088a8f1b351d75229399169835e
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-01-20 12:44:58 +0100

    Add method for positions

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -5,6 +5,9 @@ module TicTacToe
         private
             @@pos = 0
             @@array_symbols = Array.new(3) {Array.new()}
+            @@array_of_valid_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            @@posX = 0
+            @@posY = 0
         public
         def newGame
             Grid.clearGrid
@@ -12,12 +15,38 @@ module TicTacToe
         def addSymbol(x, y, s)
             Grid.ttt(x, y, s)
         end
+        def positionAssignment(pos)
+            case pos.to_i
+                when 1..3
+                    @@posX = 0
+                when 4..6
+                    @@posX = 1
+                when 7..9
+                    @@posX = 2
+            end
+            case ((pos.to_i + 3)%3)
+            when 0
+                @@posY = 2
+            when 1
+                @@posY = 0
+            when 2 
+                @@posY = 1
+            end
+            p "Position x #{@@posX}, Position y #{@@posY}"
+        end
         def selectPosition
-            puts 'Select position from 1 to 9'
+            puts 'Select position'
             @@pos = gets.chomp
             until @@pos.to_i.between?(1, 9)
-                puts "Wrong selection!!!\nSelect again"
+                puts "Select again:\n"
                 @@pos = gets.chomp
+            end
+            @@array_of_valid_positions.each_with_index do
+                |val, ind|
+                if val == @@pos.to_i
+                    @@array_of_valid_positions[ind] = 0
+                    positionAssignment(@@pos)
+                end
             end
         end
     end
