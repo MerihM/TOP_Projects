0aa6b88c273a4d18d4a9e71b9a047f60350598ef
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-04 12:55:03 +0100

    Add check for bad numbers

--- clean_phone_numbers/lib/clean_number.rb
+++ clean_phone_numbers/lib/clean_number.rb
@@ -4,5 +4,8 @@ contents = CSV.open('event_attendees.csv', headers: true, header_converters: :sy
 
 contents.each do |line|
     number = line[:homephone]
+    lastname = line[:last_name]
+    name = line[:first_name]
     cleaner_number = number.gsub(/[^0-9]/, '')
+    puts "Error! #{name} #{lastname} has bad number, it has #{cleaner_number.length} digits" if (cleaner_number.length >= 11 && cleaner_number[0] != '1') || cleaner_number.length < 10
 end
