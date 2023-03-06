2b6e678c0d8f8692622e1b29f362ba16646cd357
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-06 17:44:03 +0100

    Count hours

--- time_targeting/lib/time_targeting.rb
+++ time_targeting/lib/time_targeting.rb
@@ -1,10 +1,12 @@
 require 'csv'
 require 'date'
 
-contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
-date_format = "%-m/%d/%Y"
+contents = CSV.open('event_attendees_full.csv', headers: true, header_converters: :symbol)
+count = Hash.new(0)
 contents.each do 
     |f|
     test_date = DateTime.strptime(f[:regdate], "%m/%d/%Y %k:%M")
-    puts test_date.hour
+    count[test_date.hour] += 1
 end
+count = count.sort_by{|h, num| num}
+p count
