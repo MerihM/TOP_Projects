0089c757259eb3146024ebe04e52b19cadec71a5
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-06 17:22:34 +0100

    Parse date and time

--- time_targeting/lib/time_targeting.rb
+++ time_targeting/lib/time_targeting.rb
@@ -1,7 +1,10 @@
 require 'csv'
+require 'date'
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+date_format = "%-m/%d/%Y"
 contents.each do 
     |f|
-    puts f[:regdate]
+    test_date = DateTime.strptime(f[:regdate], "%m/%d/%Y %k:%M")
+    puts test_date.hour
 end
