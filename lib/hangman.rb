46a2f73d37e8fa61855809ef6398653fe944b6d6
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-07 16:27:30 +0100

    Make Hangman class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,28 +1,43 @@
-def word_length_condition? (word)
-    word.length >= 5 && word.length <= 12 
-end
+class Hangman
+    attr_reader :word
+    @word
 
-def load_words
-    content = File.open('google_10000_english.txt')
-    arr_of_words = []
-    while !content.eof?
-        line = content.readline.strip
-        arr_of_words.push(line) if word_length_condition?(line)
+    def set_word
+        @word = get_random_word
     end
-    arr_of_words
+
+    private 
+
+    def  word_length_condition? (word)
+        word.length >= 5 && word.length <= 12
+    end
+
+    def  load_words
+        content = File.open('google_10000_english.txt')
+        arr_of_words = []
+        while !content.eof?
+            line = content.readline.strip
+            arr_of_words.push(line) if word_length_condition?(line)
+        end
+        arr_of_words
+    end
+
+    def  get_random_word
+        load_words[rand(0..load_words.length)]
+    end
+
+    def  get_user_input
+        puts 'Enter some word'
+        word = gets.chomp.downcase
+    end
+
+    def  compare_words?
+        get_user_input == @word
+    end
+    
 end
 
-def get_random_word
-    load_words[rand(0..load_words.length)]
-end
+h = Hangman.new
+h.set_word
+p h.word
 
-def get_user_input
-    puts 'Enter some word'
-    word = gets.chomp.downcase
-end
-
-def compare_words?
-    get_user_input == get_random_word
-end
-
-p "Your guess is #{compare_words?}"
