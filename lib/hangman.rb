d5d91148382f5478c2beafb9715a32b08ec2b741
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-07 19:52:09 +0100

    Add method to set values

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,12 +1,32 @@
 class Hangman
     attr_reader :word
     @word
+    @user_input
+    @input_array
 
     def set_word
         @word = get_random_word
+        @input_array = []
+        @word.length.times do
+            @input_array.push('_ ')
+        end
     end
 
+    def check_value_vars
+        p "Word : #{@word}"
+        p "User input : #{@user_input}"
+        p "Number of letters : #{@input_array}"
+    end
+    def play_round
+        check_input
+    end
     private 
+
+    def  compare_words?
+        p @user_input
+        p @word
+        p @user_input == @word
+    end
 
     def  word_length_condition? (word)
         word.length >= 5 && word.length <= 12
@@ -31,13 +51,33 @@ class Hangman
         word = gets.chomp.downcase
     end
 
-    def  compare_words?
-        get_user_input == @word
+    def one_letter_check
+        p 'test'
     end
-    
+
+    def save_game
+        p 'testSave'
+    end
+    def check_input
+
+        @user_input = get_user_input
+        length_of_input = @user_input.length
+
+        case length_of_input
+        when 1 then one_letter_check
+            
+        when 4 then save_game
+            
+        else compare_words?
+            
+        end
+    end
 end
 
 h = Hangman.new
 h.set_word
+
 p h.word
+h.play_round
+h.check_value_vars
 
