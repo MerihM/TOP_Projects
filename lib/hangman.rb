fc0351f691107ac6b89b6fc9868bb33e9192df19
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-07 15:04:35 +0100

    Enable user input

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,9 +2,20 @@ def word_length_condition? (word)
     word.length >= 5 && word.length <= 12 
 end
 
-content = File.open('google_10000_english.txt')
-arr_of_words = []
-while !content.eof?
-    line = content.readline.strip
-    arr_of_words.push(line) if word_length_condition?(line)
+def load_words
+    content = File.open('google_10000_english.txt')
+    arr_of_words = []
+    while !content.eof?
+        line = content.readline.strip
+        arr_of_words.push(line) if word_length_condition?(line)
+    end
+    arr_of_words
 end
+
+def get_user_input
+    puts 'Enter some word'
+    word = gets.chomp.downcase
+    word
+end
+
+
