4d0ea5bed236c98c3d318f8e4ad4fd1056b99b9b
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-18 04:04:50 +0100

    Rewrite save method

--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,34 @@
 require "yaml"
-require "class_hangman.rb"
+require_relative "hangman"
 
 def save_game(game)
-
     dir_name = 'save_games'
     Dir.mkdir(dir_name) unless Dir.exists?(dir_name)
-    no_of_saves = Dir.glob("#{dir_name}/*.{yml}").length
     puts 'Enter the name of the save'
     save_name = gets.chomp
-    File.open("#{dir_name}/#{no_of_saves + 1}. #{save_name}.yml", "w") do |f|
+    File.open("#{dir_name}/#{save_name}.yml", "w") do |f|
         f.write YAML.dump(game)
     end
-
 end
 
-def chose_game
-
+def save_helper game
+    puts `clear`
+    puts "Saving game..."
+    save_game(game)
+    puts `clear`
+    puts 'Game saved! Press any key to continue...'
+    gets
+    puts `clear`
 end
-def load_game
 
-end
 
 h1 = Hangman.new
 until h1.over?
-    p 'save game' if check_input == 'save'
+    if h1.check_input == 'save'
+        save_helper(h1)
+        puts "Do you want to continue playing? (y/n)"
+        continue = gets.chomp
+        return if continue.downcase == 'n' || continue.downcase == 'no'
+        puts `clear`
+    end
 end
