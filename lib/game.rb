1c8dc213e40a2e59541f2539e4240694472322ae
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-03-18 04:43:40 +0100

    Rewrite whole load_game method

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@ def save_game(game)
     Dir.mkdir(dir_name) unless Dir.exists?(dir_name)
     puts 'Enter the name of the save'
     save_name = gets.chomp
-    File.open("#{dir_name}/#{save_name}.yml", "w") do |f|
+    File.open("#{dir_name}/#{save_name}.yaml", "w") do |f|
         f.write YAML.dump(game)
     end
 end
@@ -21,22 +21,31 @@ def save_helper game
     puts `clear`
 end
 
-def load_game 
+def choose_save 
     dir_name = 'save_games'
-    unless Dir.exists?(dir_name) || Dir["#{dir_name}/*"].length != 0
+    unless Dir.exists?(dir_name) && Dir["#{dir_name}/*"].length != 0
         puts "There are no saved games"
         return 
     end
+    filenames = Dir.glob("#{dir_name}/*").map { |file| file[(file.index('/') + 1)...(file.index('.'))] }
+    puts "Choose saved game"
+    puts filenames
+    begin
+        fn = gets.chomp
+        raise "#{fn} doesn't exist" unless filenames.include?(fn)
+        puts "Loading #{fn}...\n"
+        "#{dir_name}/#{fn}.yaml"
+    rescue StandardError => e
+        puts e
+        retry
+    end
 end
 
-load_game
-# h1 = Hangman.new
-# until h1.over?
-#     if h1.check_input == 'save'
-#         save_helper(h1)
-#         puts "Do you want to continue playing? (y/n)"
-#         continue = gets.chomp
-#         return if continue.downcase == 'n' || continue.downcase == 'no'
-#         puts `clear`
-#     end
-# end
+def load_game 
+    fn = choose_save
+    return if fn == nil
+    saved = File.open(File.join(Dir.pwd, fn), 'r')
+    loaded_game = YAML.unsafe_load(saved)
+    saved.close
+    puts loaded_game
+end
