5ff3614078a0ca7ea02b66986277817c940eba5f
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-04-12 12:12:19 +0200

    Code cleanup

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -2,13 +2,11 @@ def merge_sort (arr)
     return arr if arr.length <= 1
     full = arr.length
     half = full/2
-    if arr.length > 1 
+    if full > 1 
         a1 = merge_sort(arr[0...half])
         a2 = merge_sort(arr[half..full])
         a3 = []
-        while a1.length >= 1 && a2.length >= 1 
-            a1[0] < a2[0] ? a3 << a1.shift : a3 << a2.shift
-        end
+        a1[0] < a2[0] ? a3 << a1.shift : a3 << a2.shift while a1.length >= 1 && a2.length >= 1 
         a1.length > 0 ? a3 << a1 : a3 << a2
         a3.flatten  
     else 
@@ -17,4 +15,4 @@ def merge_sort (arr)
 end
 
 
-p merge_sort [2, 3, 1, 7, 8, 4, 2]
+p merge_sort [12, 2, 3, 1, 7, 8, 4, 2]
