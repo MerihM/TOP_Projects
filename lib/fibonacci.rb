c9f3d0a12f2a6dc6eb77de68945200c23242e5c1
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-04-12 10:36:18 +0200

    Refactor code

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,7 +1,7 @@
 def fibs (num_of_arr_elements)
     fib_arr = [0]
-    for i in 1..num_of_arr_elements
-        fib_arr.length == 1 ? fib_arr << 1 : fib_arr << (fib_arr[-1] + fib_arr[-2])
-    end
-    p fib_arr[0..-2]
+    (num_of_arr_elements).times {fib_arr.length == 1 ? fib_arr << 1 : fib_arr << (fib_arr[-1] + fib_arr[-2])}
+    fib_arr[0..-2]
 end
+
+p fibs 8
