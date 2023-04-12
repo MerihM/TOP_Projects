15e8ebc94fe182703117c51ed2a8594717fbca59
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-04-12 12:08:31 +0200

    Implement merge sort

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,8 +1,20 @@
 def merge_sort (arr)
-    return if arr.length < 2
-    merge_sort(arr[0...arr.length/2])
-    merge_sort(arr[arr.length/2..arr.length])
-
+    return arr if arr.length <= 1
+    full = arr.length
+    half = full/2
+    if arr.length > 1 
+        a1 = merge_sort(arr[0...half])
+        a2 = merge_sort(arr[half..full])
+        a3 = []
+        while a1.length >= 1 && a2.length >= 1 
+            a1[0] < a2[0] ? a3 << a1.shift : a3 << a2.shift
+        end
+        a1.length > 0 ? a3 << a1 : a3 << a2
+        a3.flatten  
+    else 
+        arr
+    end
 end
 
-merge_sort [2, 3, 1]
+
+p merge_sort [2, 3, 1, 7, 8, 4, 2]
