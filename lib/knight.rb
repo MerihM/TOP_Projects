4146f7ca4e91336d289b22a8df54898cc1ca2ce0
Merih_M <103122972+MerihM@users.noreply.github.com>
2023-05-22 18:37:19 +0200

    Implement initialization for knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,10 +1,24 @@
 require_relative "./graph"
 class Knight
 
-    attr_accessor :current, :target
+    attr_accessor :current, :target, :garph
 
-    def initialize(cur = [0, 0], tar = [0, 0])
-        @current = cur
-        @target = tar
+
+    def initialize(current = [0, 0], target = [0, 0])
+        @current = current
+        @target = target
+        @graph = Graph.new()
+        @graph.add_node(@current)
+    end
+
+    def calculate_next_positions(current_pos)
+
+    end
+
+    def print_path
+        @graph.find_path(@current)
     end
 end
+
+test = Knight.new([1, 1], [0, 0])
+p test.print_path
