c97100818e022d3781ebb7f1875e9f7c9ef44100
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-25 17:32:17 +0200

    Enable tests for add_token

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,12 +3,12 @@ require './lib/board'
 describe Board do
    describe '#add_token' do
     let(:test_arr) {Array.new(6){Array.new(7){'i'}}}
-        xit "add token to the position in the board" do
+        it "add token to the position in the board" do
             test_arr[5][4] = 'x'
             board = Board.new
             expect(board.add_token('x', 5)).to eql(test_arr)
         end
-        xit "add tokens to the positions in the board" do
+        it "add tokens to the positions in the board" do
             test_arr[5][4] = 'x'
             board = Board.new
             expect(board.add_token('x', 5)).to eql(test_arr)
