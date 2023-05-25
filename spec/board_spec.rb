48c13296bd39fdefe797e83f5961359773635752
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-25 17:40:53 +0200

    Add test for full column

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,5 +15,21 @@ describe Board do
             test_arr[4][4] = 'o'
             expect(board.add_token('o', 4)).to eql(test_arr)
         end
+        it "expect error message for full column" do
+            test_arr[5][4] = 'x'
+            board = Board.new
+            expect(board.add_token('x', 4)).to eql(test_arr)
+            test_arr[4][4] = 'o'
+            expect(board.add_token('o', 4)).to eql(test_arr)
+            test_arr[3][4] = 'o'
+            expect(board.add_token('o', 4)).to eql(test_arr)
+            test_arr[2][4] = 'o'
+            expect(board.add_token('o', 4)).to eql(test_arr)
+            test_arr[1][4] = 'o'
+            expect(board.add_token('o', 4)).to eql(test_arr)
+            test_arr[0][4] = 'o'
+            expect(board.add_token('o', 4)).to eql(test_arr)
+            expect(board.add_token('x', 4)).to eql("That column is full!!!")
+        end
    end 
 end
