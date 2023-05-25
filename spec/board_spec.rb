1251b29bc8c31d24f53b02255eb3f1baa6cdd415
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-25 17:34:39 +0200

    Bug fix in test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,14 +6,14 @@ describe Board do
         it "add token to the position in the board" do
             test_arr[5][4] = 'x'
             board = Board.new
-            expect(board.add_token('x', 5)).to eql(test_arr)
+            expect(board.add_token('x', 4)).to eql(test_arr)
         end
         it "add tokens to the positions in the board" do
             test_arr[5][4] = 'x'
             board = Board.new
-            expect(board.add_token('x', 5)).to eql(test_arr)
+            expect(board.add_token('x', 4)).to eql(test_arr)
             test_arr[4][4] = 'o'
-            expect(board.add_token('o', 5)).to eql(test_arr)
+            expect(board.add_token('o', 4)).to eql(test_arr)
         end
    end 
 end
