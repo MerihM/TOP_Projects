f5ed6084777cf7f6cdccd2ba648fcfd76ecac00e
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-25 17:21:35 +0200

    Implement add_token method

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,26 @@ class Board
     attr_accessor :board_arr
 
     def initialize 
-        @board_arr = Array.new(6){Array.new(7)} 
+        @board_arr = Array.new(6){Array.new(7) {"i"}} 
+    end
+
+    def add_token(token, position)
+        5.downto(0) do 
+            |i|
+            if board_arr[i][position] == 'i'
+                board_arr[i][position] = token 
+                break
+            end
+        end
     end
 end
+
+brd = Board.new
+brd.add_token('a', 4)
+brd.add_token('x', 4)
+brd.add_token('a', 4)
+brd.add_token('x', 4)
+brd.add_token('a', 4)
+brd.add_token('x', 4)
+brd.add_token('a', 4)
+p brd.board_arr
