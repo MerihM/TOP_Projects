4b6dc9b624ae0ceb4425338d22b9e05c0841de93
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-30 16:03:21 +0200

    Add check_win method

--- lib/connect.rb
+++ lib/connect.rb
@@ -1,12 +1,13 @@
 require_relative './board'
 require 'io/console'
 class Connect
-    attr_accessor :player_one_status, :player_one_token, :player_two_token, :board
+    attr_accessor :player_switch, :player_one_token, :player_two_token, :board, :win
     def initialize
-        @player_one_status = true
+        @player_switch = true
         @player_one_token = 'x'
         @player_two_token = 'o'
         @board = Board.new
+        @win = false
     end    
 
     def user_input
@@ -24,19 +25,41 @@ class Connect
 
     def play_round(position = user_input) 
         system "clear"
-        @player_one_status ? token = player_one_token : token = player_two_token
+        @player_switch ? token = player_one_token : token = player_two_token
         if board.add_token(token, position-1) == "That column is full!!!"
             play_round
             return
         end
         board.print_board
-        @player_one_status = !player_one_status
+        @player_switch = !player_switch
+    end
+
+    def check_win
+
+        direction = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+        consecutive_tokens = 0
+
+        board.board_arr.each do
+            |arr|
+            consecutive_tokens = horizontal_check(arr, consecutive_tokens)
+            return consecutive_tokens if consecutive_tokens == 4
+        end
+        # vertical_check
+        # diagonal_check
+
+    end
+
+    def horizontal_check(arr, ctr)
+
+        p arr
+        4
     end
 
 end
 
 game = Connect.new
-4.times{game.play_round}
+# 4.times{game.play_round}
+game.check_win
 
 
 
