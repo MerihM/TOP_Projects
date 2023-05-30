ac0e51382843eae330ef8e367fad6bde929dd0fc
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-30 15:03:03 +0200

    Add instance variables

--- lib/connect.rb
+++ lib/connect.rb
@@ -1,4 +1,13 @@
 require_relative './board'
 class Connect
+    attr_accessor :player_one_status, :player_one_token, :player_two_token, :board
+    def initialize
+        @player_one_status = true
+        @player_one_token = 'x'
+        @player_two_token = 'o'
+        @board = Board.new
+    end    
+
     
+
 end
