1042e87c21e7f55ef046b9773f43c0ff22230560
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-30 21:41:24 +0200

    Add moves for pawn, knight and king

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,13 +1,20 @@
 class Pawn
-
+    attr_accessor :move
+    def initialize
+        @move = [0, 1]
+    end
 end
 
 class Rook
-
+    attr_accessor :move
+    
 end
 
 class Knight
-    
+    attr_accessor :move
+    def initialize
+        @move = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
+    end
 end
 
 class Bishop 
@@ -19,5 +26,8 @@ class Queen
 end
 
 class King 
-    
+    attr_accessor :move
+    def initialize
+        @move = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+    end
 end
