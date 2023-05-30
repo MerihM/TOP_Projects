61330dcd34a1d220192d45d419a3a95b594801be
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-30 15:30:27 +0200

    Add user_input method

--- lib/connect.rb
+++ lib/connect.rb
@@ -1,4 +1,5 @@
 require_relative './board'
+require 'io/console'
 class Connect
     attr_accessor :player_one_status, :player_one_token, :player_two_token, :board
     def initialize
@@ -7,6 +8,19 @@ class Connect
         @player_two_token = 'o'
         @board = Board.new
     end    
+
+    def user_input
+        system "clear"
+        board.print_board
+        puts "\n\t\tEnter a position on the board"
+        pos = gets.chomp!.to_i
+        if !pos.between?(1, 7)
+            puts "Wrong input!!! Press any key to repeat input"
+            STDIN.getch
+            pos = user_input
+        end
+        pos
+    end
 
     def play_round(position = rand(7)) # has to take user input for position argument, rand for testing
         system "clear"
@@ -22,8 +36,8 @@ class Connect
 end
 
 game = Connect.new
-55.times{game.play_round}
-
+# 55.times{game.play_round}
+p game.user_input
 
 
 
