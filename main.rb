88701d2106c76595da5cb61e86ee846b91e07085
MerihM <103122972+MerihM@users.noreply.github.com>
2023-05-30 19:09:05 +0200

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative 'lib/board'
+require_relative 'lib/connect'
+require 'io/console'
+
+def new_game 
+    game = Connect.new
+    round_count = 0
+    while round_count < 42
+        game.play_round
+        game.check_win
+        break if game.win 
+        round_count+=1
+        p round_count
+        STDIN.getch
+    end
+end
