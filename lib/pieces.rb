1212202334fbb936ee6e10186f7ea0fe4b610307
MerihM <103122972+MerihM@users.noreply.github.com>
2023-06-02 16:15:49 +0200

    Add Knight class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -129,3 +129,33 @@ class Queen < Piece
         super
     end
 end
+
+class Knight < Piece
+
+    def initialize(position, is_white)
+        @moveset = [
+            [1, 2],
+            [1, -2],
+            [-1, 2],
+            [-1, -2],
+            [2, 1],
+            [2, -1],
+            [-2, 1],
+            [-2, -1]
+        ]
+        @icon = is_white ? '♞' : '♘'
+        super
+    end
+
+    def find_possible_moves(positions)
+        @possible_moves = []
+        @moveset.each do 
+            |move|
+            x = @x + move[0]
+            y = @y + move[1]
+            if on_board?(x, y)
+                @possible_moves << [x, y] if positions[x][y].nil? || positions[x][y].color != @color
+            end
+        end
+    end
+end
