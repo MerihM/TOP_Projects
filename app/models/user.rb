de6f230552f167f45a37ebddaf31dc219a976ebc
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-08 00:24:48 +0200

    Add some validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, length: {in: 5..50, message: 'Minimum 5 characters'}
+    validates :password, length: {minimum: 7, message: 'Minimum 7 characters'}
+    # validates :password, format: { with: /\[A-Z]+\/, message: 'regex validation test'}
 end
