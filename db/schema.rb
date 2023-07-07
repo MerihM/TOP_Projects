e00d135eb45d37e3ef9dc0378b065a97023e1c03
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-08 01:30:36 +0200

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_215154) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_232833) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password", null: false
