51b81603111a9a8615d5529c92b5b7e77b6bf5ed
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-08 01:48:02 +0200

    Rename db column

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_233221) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_234344) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "users_id"
-    t.index ["users_id"], name: "index_posts_on_users_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,5 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_233221) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "posts", "users", column: "users_id"
+  add_foreign_key "posts", "users"
 end
