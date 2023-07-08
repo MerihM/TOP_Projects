8f3c0ab7fffc11894ae82110f0f6ff99bfb4d2dd
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-08 03:01:54 +0200

    Bug fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
     validates :user_id, presence: true
     validates :post_id, presence: true
-    
-    belongs_to :users
-    belongs_to :posts
+
+    belongs_to :user
+    belongs_to :post
 end
