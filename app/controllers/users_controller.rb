ac7d46c2a3fc53f32146866c739d2c1e6eaf087d
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-11 22:32:14 +0200

    Add routes for update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,16 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+
+    end
+
+    def update 
+
+    end
+    
+    private
+
     def user_params 
         params.require(:user).permit(:username, :email, :password)
     end
