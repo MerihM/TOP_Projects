01e1b24ea735960cf2e716618bc96c8b3c1cd07d
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-13 19:21:51 +0200

    Add unique index to email and username

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_171419) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_171737) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
