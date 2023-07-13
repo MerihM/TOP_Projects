c06f0abde21a768533060b4f963e1b2cf14db5be
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-13 20:08:25 +0200

    Add references to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
 
     validates :username, :email, :password, presence: true, uniqueness: true
     validates :password, length: { minimum: 10 }, uniqueness: true
+
+    has_many :posts
 end
