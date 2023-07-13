c4c71758a44fe0dbd150aaa3e277f8be6cb37217
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-13 19:31:11 +0200

    Add error handling for uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 
-    validates :username, :email, :password, presence: true
-    validates :password, length: { minimum: 10 }
+    validates :username, :email, :password, presence: true, uniqueness: true
+    validates :password, length: { minimum: 10 }, uniqueness: true
 end
