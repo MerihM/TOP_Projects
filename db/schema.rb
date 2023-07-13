00254c25a1b89e13229e178471b78f4b34181d1a
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-13 19:17:12 +0200

    Removed botched uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_163542) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_171419) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["username", "email"], name: "index_users_on_username_and_email", unique: true
   end
 
 end
