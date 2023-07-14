154f698e8cc36aec5ba091f11fbb2d0a1fd9d0ed
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-14 13:33:48 +0200

    Add posts migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_104508) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_14_113141) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_104508) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
