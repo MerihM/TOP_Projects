1be6ede5831e9d3c4e247adb49620bb7ce0b180f
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-14 13:38:26 +0200

    Add post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, :email, :password, presence: true
+  validates :password, length: {minimum: 10}
+  validates :username, :email, uniquness: true
+
+  has_many :posts
 end
