0b0c69e96d87d427b9a1b7b5ec8359d40c92b47d
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-20 17:08:07 +0200

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :contents, only: [:new, :create, :index]
   devise_for :users, :controllers => { registrations: 'users/registrations' }
-  root 'users#index'
+  root 'contents#index'
   resources :users
   # resources :posts, only: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
