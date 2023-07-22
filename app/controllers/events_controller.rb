f3ebdaf8e23395c0d2870dccc01f28d98ab56e59
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-22 13:46:27 +0200

    Add events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,25 @@ class EventsController < ApplicationController
     def index 
         @events = Event.all
     end
+
+    def show 
+        @event = Event.find(params[:id])
+    end
+
+    def new 
+        @event = Event.new
+    end
+
+    def create 
+        @event = current_user.events.build(event_params)
+
+        if @event.save
+            flash[:success] = "Event '#{@event.title}' created!"
+            redirect_to @event
+        else
+            flash[:alert] = "Thera was an error"
+            render 'new'
+        end
+    end
+    
 end
