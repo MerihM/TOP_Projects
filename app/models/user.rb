0e5069a55a29db80f638d937c6e415f8f37f63bb
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-22 16:35:32 +0200

    Delete user posts on user delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :events, inverse_of: :creator
+         has_many :events, inverse_of: :creator, :dependent => :destroy
          has_many :events_users
          has_many :attended_events, through: :events_users, source: :event
 end
