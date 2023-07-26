9036d627545b35a7bdead98ff4536a7d6e4c4c43
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-26 17:24:51 +0200

    Add routes for attendees

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events
   root 'events#index'
+  resources :attendees, only: [:create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
