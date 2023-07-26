7446600fc6c963d9c29c9dda5693cf74d02196ae
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-26 17:05:13 +0200

    Add attendees table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_144352) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_150403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendees", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.index ["event_id"], name: "index_attendees_on_event_id"
+    t.index ["user_id"], name: "index_attendees_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -37,5 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_144352) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attendees", "events"
+  add_foreign_key "attendees", "users"
   add_foreign_key "events", "users"
 end
