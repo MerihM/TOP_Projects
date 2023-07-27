210a2160d5990f8b3c2409118f723a338404f12f
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-27 15:01:33 +0200

    Add removeBook method

--- library.js
+++ library.js
@@ -14,14 +14,21 @@ function Library() {
     }
 
     this.inLibrary = function (checkBook) {
-        return this.books.some((book) => book.name === checkBook.name)
+        return this.books.some((book) => book.name === checkBook.name);
+    }
+
+    this.removeBook = function (title) {
+        this.books = this.books.filter((book) => book.name !== title)
     }
 }
 
 hobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
-hobbit2 = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
+hobbit2 = new Book("Hobbit1", "J.R.R. Tolkien", "295", true);
 library = new Library;
 library.addBook(hobbit);
 library.addBook(hobbit2);
 
+console.log(library.books);
+
+library.removeBook("Hobbit1");
 console.log(library.books);
