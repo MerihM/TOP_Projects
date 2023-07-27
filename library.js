1e02f1781ba16865de3254d7ef18226a08705d30
MerihM <103122972+MerihM@users.noreply.github.com>
2023-07-27 16:57:14 +0200

    Add method for adding to library

--- library.js
+++ library.js
@@ -30,12 +30,28 @@ hobbit = new Book('The Hobbit', "J.R.R. Tolkien", 295, false)
 let library = new Library
 library.addBook(hobbit);
 const lib = document.querySelector('.library');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const bookStatus = document.querySelector('#status');
 const listOfBooks = library.books;
 function listBooks() {
     for (const book of listOfBooks) {
         console.log(book)
         lib.innerHTML += card(book.name, book.author, book.pages, book.status);
     }
+}
+
+bookTitle.addEventListener('keydown', (e) => write(e))
+function write(e) {
+    console.log(e);
+    console.log(bookStatus.value);
+}
+
+function addToLibrary() {
+    newBook = new Book(bookTitle.value, bookAuthor.value, bookPages, bookStatus);
+    library.addBook(newBook);
+    listBooks;
 }
 
 function card(title, author, pages, status) {
